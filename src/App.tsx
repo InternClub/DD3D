2875efc877c7f6c7586b8a2d740dd3ff0f83f7b4
Devansh Kumar <devanshkumar6298@gmail.com>
2025-07-19 11:04:31 +0530

    added css

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import { BrowserRouter,Route,Routes } from 'react-router'
 
 import './App.css'
 import Home from './pages/Home/Home'
-import bgImg from '/Images/bgImg.png'
+import Layout from './pages/Layout/Layout'
 
 function App() {
   return (
     <div >
 
-   
       <img src={bgImg} alt="Background" className='w-[100vw] h-[100vh] object-fit fixed -z-5' />
+   
 
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Layout/>} />
+          <Route  path='home' index element={<Home />} />
           {/* <Route path="/about" element={<About />} /> */}
         </Routes>
       </BrowserRouter>
